56aedfdce431e6abc64b516f783e8a12077cb6cd
lucasmarfe <lucasmarfe@gmail.com>
2016-04-02 12:25:30 -0300

    Progresso

--- app/src/main/java/com/felipe/lucas/lfwframework/Screen/BaseAboutScreen.java
+++ app/src/main/java/com/felipe/lucas/lfwframework/Screen/BaseAboutScreen.java
@@ -5,32 +5,30 @@ import android.os.Bundle;
 import com.felipe.lucas.lfwframework.Components.LabelValueLFW;
 import com.felipe.lucas.lfwframework.Util.UtilLFW;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * Created by lucas on 25/03/2016.
+ * Created by lucas on 31/03/2016.
  */
-public class BaseConfigurationScreen extends BaseScreenWithHeader {
+public class BaseAboutScreen extends BaseScreenWithHeader {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         buildPage();
-        m_IFrame.builScreen(this.getClass(), "Configurações");
+        m_IFrame.builScreen(this.getClass(), "Sobre");
         setContentView(m_IFrame);
     }
 
     public void buildPage () {
         TreeMap<String, String> v_ConfigList = UtilLFW.getConfigTreeMap();
-        for (Map.Entry<String, String> v_Item : v_ConfigList.entrySet()) {
-            LabelValueLFW v_ItemLabelValue = new LabelValueLFW(getApplicationContext(), v_Item
-                    .getKey(), v_Item.getValue(), true);
-            v_ItemLabelValue.setValuePadding(10, 50, 0, 50);
+        String[] v_Lines = v_ConfigList.get("AboutLine").split(";");
+        for (String v_Value : v_Lines) {
+            LabelValueLFW v_ItemLabelValue = new LabelValueLFW(getApplicationContext(), v_Value,
+                    null, false);
+            v_ItemLabelValue.setValuePadding(10, 50, 0, 0);
             m_IFrame.add(v_ItemLabelValue);
         }
-
     }
 }
